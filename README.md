cff68f4df39b3c0452f33307fef7c44290223146
glydzo <loch.ewen2@gmail.com>
2021-06-23 11:41:31 +0200

    Adding the files.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# CNN-on-GPU
+Start terminal and remove any NVIDIA traces you may have on your machine.
+
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt remove --autoremove nvidia-cuda-toolkit
+sudo apt remove --autoremove nvidia-*
+
+sudo apt update
+
+sudo add-apt-repository ppa:graphics-driverssudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+
+sudo apt update
+sudo apt install cuda-10-0
+sudo apt install libcudnn7
+
+sudo vi ~/.profile
+
+# set PATH for cuda 10.1 installation
+if [ -d "/usr/local/cuda-10.1/bin/" ]; then
+    export PATH=/usr/local/cuda-10.1/bin${PATH:+:${PATH}}
+    export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+fi
+
+Restart and check the versions for the installation.
+
+CUDA:
+
+nvcc  – versionnvcc: NVIDIA (R) Cuda compiler driver
+Copyright (c) 2005-2019 NVIDIA Corporation
+Built on Wed_Apr_24_19:10:27_PDT_2019
+Cuda compilation tools, release 10.1, V10.1.168
+
+NVIDIA Driver:
+
+nvidia-smi
+
+libcudnn:
+
+/sbin/ldconfig -N -v $(sed ‘s/:/ /’ <<< $LD_LIBRARY_PATH) 2>/dev/null | grep libcudnn
+
+```bash
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ src/runCNN.py
@@ -0,0 +1,61 @@
+import os
+import time
+import numpy as np
+# import wandb
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+# 0 = all messages are logged (default behavior)
+# 1 = INFO messages are not printed
+# 2 = INFO and WARNING messages are not printed
+# 3 = INFO, WARNING, and ERROR messages are not printed
+
+import tensorflow as tf
+
+# disable tf warnings
+tf.get_logger().setLevel('ERROR')
+
+# initialize wandb
+# run = wandb.init(project='cnn_optimized_execution')
+
+# set memory growth
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# loading the model
+model = tf.keras.models.load_model('../res/models/my_model_tech_db_filtered2020-05-13_15-05-43_0.094_0.094.h5')
+
+# loading the image
+image = np.load('../res/dataset/1000008.npy')
+
+# init of qp and times list
+qp = 22
+
+elapsed_times = []
+
+# running multiple inferences
+for i in tqdm(range(10000)):
+
+	start_time = time.time()
+
+	prediction = model.predict([[image],[qp]])
+
+	end_time = time.time()
+
+	elapsed_times.append((end_time - start_time) * 1000)
+
+print('Inferences finished ! Dimensions : ', prediction.shape, ' / Average execution time : %.3f ms ' % (sum(elapsed_times)/len(elapsed_times)), sep="")
+
+# run.finish()
+
+plt.plot(elapsed_times)
+plt.title('Evolution of inference times')
+plt.xlabel('Iterations')
+plt.ylabel('Execution time')
+
+plt.show()
+
+
+
